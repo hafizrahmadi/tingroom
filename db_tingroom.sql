a7317ae68d439964054f7c582b4c332405638aea
Hafiz Rahmadi <hafiz.rahmadi@gmail.com>
2017-05-12 16:12:55 +0700

    Perbaikan database

--- db_tingroom.sql
+++ db_tingroom.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2017 at 10:24 AM
+-- Generation Time: May 12, 2017 at 11:12 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -29,18 +29,16 @@ USE `db_tingroom`;
 -- Table structure for table `tb_booking`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_booking` (
-  `id_booking` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tb_booking`;
+CREATE TABLE `tb_booking` (
+  `id_booking` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_jadwal` int(11) NOT NULL,
   `waktu` date NOT NULL,
   `jenis` varchar(100) NOT NULL,
   `deskripsi` text NOT NULL,
   `status` int(11) NOT NULL,
-  `time_created` datetime NOT NULL,
-  PRIMARY KEY (`id_booking`),
-  KEY `fk_id_jadwal` (`id_jadwal`),
-  KEY `fk_id_user` (`id_user`)
+  `time_created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -49,12 +47,11 @@ CREATE TABLE IF NOT EXISTS `tb_booking` (
 -- Table structure for table `tb_det_booking`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_det_booking` (
+DROP TABLE IF EXISTS `tb_det_booking`;
+CREATE TABLE `tb_det_booking` (
   `id_det_booking` int(11) NOT NULL,
   `id_jadwal` int(11) NOT NULL,
-  `id_booking` int(11) NOT NULL,
-  KEY `fk_id_booking` (`id_booking`),
-  KEY `fk_id_jadwal` (`id_jadwal`)
+  `id_booking` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -63,12 +60,12 @@ CREATE TABLE IF NOT EXISTS `tb_det_booking` (
 -- Table structure for table `tb_gedung`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_gedung` (
-  `id_gedung` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tb_gedung`;
+CREATE TABLE `tb_gedung` (
+  `id_gedung` int(11) NOT NULL,
   `nama_gedung` varchar(255) NOT NULL,
-  `deleted` int(11) NOT NULL,
-  PRIMARY KEY (`id_gedung`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+  `deleted` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_gedung`
@@ -88,9 +85,9 @@ INSERT INTO `tb_gedung` (`id_gedung`, `nama_gedung`, `deleted`) VALUES
 (11, 'sky eye', 1),
 (12, 'sky eyes', 1),
 (13, 'Lalalangsat Tower', 1),
-(14, '', 0),
-(15, '', 0),
-(16, '', 0);
+(14, '', 1),
+(15, '', 1),
+(16, '', 1);
 
 -- --------------------------------------------------------
 
@@ -98,17 +95,15 @@ INSERT INTO `tb_gedung` (`id_gedung`, `nama_gedung`, `deleted`) VALUES
 -- Table structure for table `tb_jadwal`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_jadwal` (
-  `id_jadwal` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tb_jadwal`;
+CREATE TABLE `tb_jadwal` (
+  `id_jadwal` int(11) NOT NULL,
   `id_ruangan` int(11) NOT NULL,
   `jam_awal` varchar(10) NOT NULL,
   `jam_akhir` varchar(10) NOT NULL,
   `status` int(11) NOT NULL,
-  `deleted` int(11) NOT NULL,
-  PRIMARY KEY (`id_jadwal`),
-  KEY `fk_id_room` (`id_ruangan`),
-  KEY `fk_id_jam` (`jam_awal`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `deleted` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_jadwal`
@@ -125,14 +120,13 @@ INSERT INTO `tb_jadwal` (`id_jadwal`, `id_ruangan`, `jam_awal`, `jam_akhir`, `st
 -- Table structure for table `tb_lantai`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_lantai` (
-  `id_lantai` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tb_lantai`;
+CREATE TABLE `tb_lantai` (
+  `id_lantai` int(11) NOT NULL,
   `nama_lantai` varchar(255) NOT NULL,
   `id_gedung` int(11) NOT NULL,
-  `deleted` int(11) NOT NULL,
-  PRIMARY KEY (`id_lantai`),
-  KEY `fk_id_gedung` (`id_gedung`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `deleted` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_lantai`
@@ -150,26 +144,55 @@ INSERT INTO `tb_lantai` (`id_lantai`, `nama_lantai`, `id_gedung`, `deleted`) VAL
 -- Table structure for table `tb_ruangan`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_ruangan` (
-  `id_ruangan` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tb_ruangan`;
+CREATE TABLE `tb_ruangan` (
+  `id_ruangan` int(11) NOT NULL,
   `nama_ruangan` varchar(255) NOT NULL,
   `id_lantai` int(11) NOT NULL,
-  `keterangan` varchar(255) DEFAULT NULL,
-  `deleted` int(11) NOT NULL,
-  PRIMARY KEY (`id_ruangan`),
-  KEY `fk_id_floor` (`id_lantai`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `board` int(11) NOT NULL,
+  `proyektor` int(11) NOT NULL,
+  `kapasitas` int(11) NOT NULL,
+  `deleted` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_ruangan`
 --
 
-INSERT INTO `tb_ruangan` (`id_ruangan`, `nama_ruangan`, `id_lantai`, `keterangan`, `deleted`) VALUES
-(1, 'Room ASD', 1, 'Fasilitas  : AC, Proyektor, Kabel LAN, Kabel Roll, Meja 2, Kursi 34', 1),
-(2, 'Room XII AB', 7, 'Fasilitas : AC, Kursi, Meja, Proyektor. Luas : 4x6m', 1),
-(3, 'Room C', 6, 'Fasilitas : Kursi dan Meja', 1),
-(4, 'Room A', 1, 'Fasilitas : AC, Proyektor, Kabel LAN, Kabel Roll, Meja 2, Kursi 3', 0),
-(5, 'Room ASD', 7, 'Fasilitas : AC, Kursi, Meja, Proyektor. Luas : 4x6m', 0);
+INSERT INTO `tb_ruangan` (`id_ruangan`, `nama_ruangan`, `id_lantai`, `board`, `proyektor`, `kapasitas`, `deleted`) VALUES
+(1, 'Room ASD', 1, 0, 0, 0, 1),
+(2, 'Room XII AB', 7, 0, 0, 0, 1),
+(3, 'Room C', 6, 0, 0, 0, 1),
+(4, 'Room A', 1, 0, 0, 0, 0),
+(5, 'Room ASD', 7, 0, 0, 0, 0),
+(6, 'Ruang Rindu', 1, 2, 1, 10, 0),
+(7, 'Ruang Hati yang terdalam', 1, 3, 1, 6, 0),
+(8, 'Ruang Hampa', 1, 1, 3, 13, 0),
+(9, 'Ruang Hompimpa', 1, 1, 3, 15, 0),
+(10, 'Ruang Rektor', 1, 1, 2, 8, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_unit`
+--
+
+DROP TABLE IF EXISTS `tb_unit`;
+CREATE TABLE `tb_unit` (
+  `id_unit` int(11) NOT NULL,
+  `nama_unit` varchar(255) NOT NULL,
+  `deleted` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_unit`
+--
+
+INSERT INTO `tb_unit` (`id_unit`, `nama_unit`, `deleted`) VALUES
+(1, 'Marketing', 0),
+(2, 'Retard', 1),
+(3, 'IT', 0),
+(4, 'HRD', 0);
 
 -- --------------------------------------------------------
 
@@ -177,28 +200,129 @@ INSERT INTO `tb_ruangan` (`id_ruangan`, `nama_ruangan`, `id_lantai`, `keterangan
 -- Table structure for table `tb_user`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `no_hp` varchar(20) DEFAULT NULL,
-  `bahasa` varchar(50) NOT NULL,
-  `floor` int(11) DEFAULT NULL,
+  `id_lantai` int(11) DEFAULT NULL,
+  `id_unit` int(11) DEFAULT NULL,
   `level` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id_user`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`id_user`, `email`, `password`, `nama`, `no_hp`, `bahasa`, `floor`, `level`, `status`) VALUES
-(1, 'admin', 'admintingroom', 'admin', NULL, 'bahasa', NULL, 1, 1),
-(2, 'hafiz.rahmadi@gmail.com', 'asdasdasd', 'Moh. Hafiz Rahmadi', '081913596746', 'bahasa', NULL, 2, 1);
+INSERT INTO `tb_user` (`id_user`, `email`, `password`, `nama`, `no_hp`, `id_lantai`, `id_unit`, `level`, `status`) VALUES
+(1, 'admin', '$2y$10$C.wMwSG2iu9dRdL9MAILiuOlhQ0yRPl0xaTDy5X8mw7hnS0m1GvxC', 'admin', NULL, NULL, NULL, 1, 1),
+(2, 'hafiz.rahmadi@gmail.com', '$2y$10$lAliR428t1bCfy24x/nvxeki6fdewu1yRQ9nizN3AkKlFhYik4AAC', 'Moh. Hafiz Rahmadi', '081913596746', NULL, NULL, 3, 1),
+(3, 'sekretaris', '$2y$10$2.ZeIP2bwJMQnqlO9Z7kme0937vPzNgYRkWs.T84R2E6WGxPK2Wwa', 'Sekretaris', NULL, NULL, NULL, 2, 1);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_booking`
+--
+ALTER TABLE `tb_booking`
+  ADD PRIMARY KEY (`id_booking`),
+  ADD KEY `fk_id_jadwal` (`id_jadwal`),
+  ADD KEY `fk_id_user` (`id_user`);
+
+--
+-- Indexes for table `tb_det_booking`
+--
+ALTER TABLE `tb_det_booking`
+  ADD KEY `fk_id_booking` (`id_booking`),
+  ADD KEY `fk_id_jadwal` (`id_jadwal`);
+
+--
+-- Indexes for table `tb_gedung`
+--
+ALTER TABLE `tb_gedung`
+  ADD PRIMARY KEY (`id_gedung`);
+
+--
+-- Indexes for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `fk_id_room` (`id_ruangan`),
+  ADD KEY `fk_id_jam` (`jam_awal`);
+
+--
+-- Indexes for table `tb_lantai`
+--
+ALTER TABLE `tb_lantai`
+  ADD PRIMARY KEY (`id_lantai`),
+  ADD KEY `fk_id_gedung` (`id_gedung`);
+
+--
+-- Indexes for table `tb_ruangan`
+--
+ALTER TABLE `tb_ruangan`
+  ADD PRIMARY KEY (`id_ruangan`),
+  ADD KEY `fk_id_floor` (`id_lantai`);
+
+--
+-- Indexes for table `tb_unit`
+--
+ALTER TABLE `tb_unit`
+  ADD PRIMARY KEY (`id_unit`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id_unit` (`id_unit`),
+  ADD KEY `id_lantai` (`id_lantai`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_booking`
+--
+ALTER TABLE `tb_booking`
+  MODIFY `id_booking` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tb_gedung`
+--
+ALTER TABLE `tb_gedung`
+  MODIFY `id_gedung` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tb_lantai`
+--
+ALTER TABLE `tb_lantai`
+  MODIFY `id_lantai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `tb_ruangan`
+--
+ALTER TABLE `tb_ruangan`
+  MODIFY `id_ruangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `tb_unit`
+--
+ALTER TABLE `tb_unit`
+  MODIFY `id_unit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
@@ -233,6 +357,13 @@ ALTER TABLE `tb_lantai`
 --
 ALTER TABLE `tb_ruangan`
   ADD CONSTRAINT `fk_id_floor` FOREIGN KEY (`id_lantai`) REFERENCES `tb_lantai` (`id_lantai`);
+
+--
+-- Constraints for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_unit`) REFERENCES `tb_unit` (`id_unit`),
+  ADD CONSTRAINT `tb_user_ibfk_2` FOREIGN KEY (`id_lantai`) REFERENCES `tb_lantai` (`id_lantai`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
